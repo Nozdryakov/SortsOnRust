98de4a41e7032269fdfd7c116133e8c05e47f01d
Nozdryakov <nozdryakovtema0603@gmail.com>
2024-01-03 03:26:38 +0200

    быстрая сортировка

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,35 @@
 use std::time::{Instant};
 use std::io;
+use rand::Rng;
+
+fn quicksort<T: Ord + std::fmt::Debug>(arr: &mut [T]) {
+    if arr.len() <= 1 {
+        return;
+    }
+
+    let pivot_index = partition(arr);
+
+    quicksort(&mut arr[..pivot_index]);
+    quicksort(&mut arr[pivot_index + 1..]);
+}
+
+fn partition<T: Ord>(arr: &mut [T]) -> usize {
+    let pivot_index = arr.len() / 2;
+    arr.swap(pivot_index, arr.len() - 1);
+
+
+    let mut i = 0;
+    for j in 0..arr.len() - 1 {
+        if arr[j] <= arr[arr.len() - 1] {
+            arr.swap(i, j);
+            i += 1;
+            
+        }
+    }
+
+    arr.swap(i, arr.len() - 1);
+    i
+}
 
 fn bubble_sort<T: Ord>(arr: &mut [T]) {
     let n = arr.len();
@@ -14,10 +44,10 @@ fn bubble_sort<T: Ord>(arr: &mut [T]) {
 }
 
 fn generate_random_vector(length: usize) -> Vec<i32> {
-    use rand::Rng;
-    
     let mut rng = rand::thread_rng();
-    (0..length).map(|_| rng.gen_range(-(length as i32)..=length as i32)).collect()
+    (0..length)
+    .map(|_| rng.gen_range(-(length as i32)..=length as i32))
+    .collect()
 }
 
 fn print_text<T: Ord>(arr: &mut [T]) {
@@ -31,59 +61,74 @@ fn main() {
 
     let choice: Result<u32, _> = input.trim().parse();
     match choice {
-        Ok(choice) => {
-            match choice {
-                1 => {
-                    println!("Введите длину вектора:");
-                    let mut length_input = String::new();
-                    io::stdin().read_line(&mut length_input).expect("Ошибка ввода");
+        Ok(1) => {
+            println!("Введите длину вектора:");
+            let mut length_input = String::new();
+            io::stdin().read_line(&mut length_input).expect("Ошибка ввода");
 
-                    let length: Result<usize, _> = length_input.trim().parse();
-                    match length {
-                        Ok(length) => {
-                            let mut numbers = generate_random_vector(length);
-                            let start_time = Instant::now();
-                            // println!("Элементы: {:?}", numbers);
-                            bubble_sort(&mut numbers);
-                            let elapsed_time = start_time.elapsed();
-                            // println!("Отсортированный массив: {:?}", numbers);
-                            
-                            // Выводим время сортировки в минутах
-                            let time_in_minutes = elapsed_time.as_millis() as f64 / 60000.0;
-                            println!("Время сортировки: {} минут", time_in_minutes);
-                        }
-                        Err(_) => {
-                            println!("Ошибка ввода числа");
-                            return;
-                        }
-                    }
+            let length: Result<usize, _> = length_input.trim().parse();
+            match length {
+                Ok(length) => {
+                    let mut numbers = generate_random_vector(length);
+                    let start_time = Instant::now();
+                    // println!("Элементы: {:?}", numbers);
+                    bubble_sort(&mut numbers);
+                    let elapsed_time = start_time.elapsed();
+                    // println!("Отсортированный массив: {:?}", numbers);
+                    
+                    // Выводим время сортировки в минутах
+                    let time_in_minutes = elapsed_time.as_millis() as f64 / 60000.0;
+                    println!("Время сортировки: {} минут", time_in_minutes);
                 }
-                2 => {
-                    println!("Введите длину вектора:");
-                    let mut length_input = String::new();
-                    io::stdin().read_line(&mut length_input).expect("Ошибка ввода");
-
-                    let length: Result<usize, _> = length_input.trim().parse();
-                    match length {
-                        Ok(length) => {
-                            let mut numbers = generate_random_vector(length);
-                            print_text(&mut numbers);
-                            println!("Элементы: {:?}", &mut numbers);
-                        }
-                        Err(_) => {
-                            println!("Ошибка ввода числа");
-                            return;
-                        }
-                    }
-                }
-                _ => {
-                    println!("Некорректный выбор, выберите 1 или 2");
+                Err(_) => {
+                    println!("Ошибка ввода числа");
                     return;
                 }
             }
         }
-        Err(_) => {
-            println!("Ошибка ввода числа");
+        Ok(2) => {
+            println!("Введите длину вектора:");
+            let mut length_input = String::new();
+            io::stdin().read_line(&mut length_input).expect("Ошибка ввода");
+
+            let length: Result<usize, _> = length_input.trim().parse();
+            match length {
+                Ok(length) => {
+                    let mut numbers = generate_random_vector(length);
+                    print_text(&mut numbers);
+                    // println!("Элементы: {:?}", &mut numbers);
+                }
+                Err(_) => {
+                    println!("Ошибка ввода числа");
+                    return;
+                }
+            }
+        }
+        Ok(3) => {
+            println!("Введите длину вектора:");
+            let mut length_input = String::new();
+            io::stdin().read_line(&mut length_input).expect("Ошибка ввода");
+
+            let length: Result<usize, _> = length_input.trim().parse();
+            match length {
+                Ok(length) => {
+                    let mut numbers = generate_random_vector(length);
+                    let start_time = Instant::now();
+                    quicksort(&mut numbers);
+                    let elapsed_time = start_time.elapsed();
+                    // println!("{:?}", &mut numbers);
+
+                    let time_in_minutes = elapsed_time.as_millis() as f64 / 60000.0;
+                    println!("Время сортировки: {} минут", time_in_minutes);
+                }
+                Err(_) => {
+                    println!("Ошибка ввода числа");
+                    return;
+                }
+            }
+        }
+        _ => {
+            println!("Некорректный выбор, выберите 1 или 2");
             return;
         }
     }
